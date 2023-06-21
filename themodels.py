da90e041dda6af5e0cf0afc457ca6a96b2c2428c
Finn <32446445+Delafinn@users.noreply.github.com>
2023-06-20 19:46:00 -0600

    Update themodels.py
    commented function call
    moved indentation. for basic use.

--- themodels.py
+++ themodels.py
@@ -1,10 +1,10 @@
 from gpt4all import GPT4All
 
+# uncomment for use as a function and indent code below.
+# def model_lister():
 
-def model_lister():
+models = GPT4All.list_models()
+filenames = [model['filename'] for model in models]
 
-    models = gpt4all.GPT4All.list_models()
-    filenames = [model['filename'] for model in models]
-
-    for filename in filenames:
-        return(filename)
+for filename in filenames:
+    print(filename)
